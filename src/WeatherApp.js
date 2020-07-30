e8f80ca42c18332db96aaff7b2c9e33375680844
jcLucio <c.lucio3@gmail.com>
2020-07-30 13:36:31 -0500

    Add initial WeatherApp boilerplate
    - Import WeatherForm/WeatherInfo

---
+++ src/WeatherApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WeatherForm from './WeatherForm';
+import WeatherInfo from './WeatherInfo';
+
+function WeatherApp() {
+  return (
+    <div>
+      <WeatherForm />
+      <WeatherInfo />
+    </div>
+  );
+}
+
+export default WeatherApp;
