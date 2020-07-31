b6b98b2a60760f589cb538c2554826284a930361
jcLucio <c.lucio3@gmail.com>
2020-07-31 11:25:59 -0500

    Add getWeather for API call

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -2,7 +2,21 @@ import React from 'react';
 import WeatherForm from './WeatherForm';
 import WeatherInfo from './WeatherInfo';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 function WeatherApp() {
+  const getWeather = async (city) => {
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+      );
+      const weatherData = await res.json();
+      console.log(weatherData);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
       <h1>WEATHER APP</h1>
