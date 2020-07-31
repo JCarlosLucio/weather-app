e99bd0738b2928b83bd1602b708f43c09faa6e0a
jcLucio <c.lucio3@gmail.com>
2020-07-31 13:39:52 -0500

    Use weather/temps props

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,16 +1,32 @@
 import React from 'react';
 
-function WeatherInfo() {
+function WeatherInfo({
+  city,
+  country,
+  weather,
+  description,
+  pressure,
+  humidity,
+  temp,
+  max,
+  min,
+  feelsLike,
+}) {
   return (
     <div>
       <h1>Weather Info</h1>
-      <h3>Weather</h3>
-      <h3>Description</h3>
+      <h3>Weather: {weather}</h3>
+      <h3>Description: {description}</h3>
       <h3>Day</h3>
-      <h3>City, Country</h3>
-      <h3>Temp MAX MIN</h3>
-      <h3>Pressure</h3>
-      <h3>Humidity</h3>
+      <h3>
+        {city}, {country}
+      </h3>
+      <h3>Feels Like: {feelsLike} </h3>
+      <h3>Temp: {temp} </h3>
+      <h3>Max: {max} </h3>
+      <h3>Min: {min} </h3>
+      <h3>Pressure: {pressure}</h3>
+      <h3>Humidity: {humidity}%</h3>
     </div>
   );
 }
