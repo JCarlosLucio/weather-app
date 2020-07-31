0d9da3403719b51d47a721339418e4db9ccf4ec6
jcLucio <c.lucio3@gmail.com>
2020-07-31 11:23:55 -0500

    Add city/setCity, update input

--- src/WeatherForm.js
+++ src/WeatherForm.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function WeatherForm() {
+  const [city, setCity] = useState('');
   return (
     <form>
       <select>
         <option>Celsius</option>
         <option>Fahrenheit</option>
       </select>
-      <input placeholder="City..." />
+      <input
+        type="text"
+        id="city"
+        placeholder="City..."
+        name="city"
+        required
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
       <button>Get weather</button>
     </form>
   );
