62083499d0aa49d2d4aeae8adea34ae970f8340a
jcLucio <c.lucio3@gmail.com>
2020-08-06 15:13:56 -0500

    Add markup for Loading

---
+++ src/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Loading() {
+  return (
+    <div>
+      <i class="far fa-sun" />
+      <h3>Consulting weather wizards...</h3>
+    </div>
+  );
+}
+
+export default Loading;
