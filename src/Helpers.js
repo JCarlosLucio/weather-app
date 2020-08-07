265567f23de06fd028263157917a19d12872a32c
jcLucio <c.lucio3@gmail.com>
2020-08-07 18:23:34 -0500

    Add getIconClass to Helpers

--- src/Helpers.js
+++ src/Helpers.js
@@ -38,3 +38,27 @@ export const makeDate = () => {
     today.getMonth()
   ]} ${today.getDate()}, ${today.getFullYear()} `;
 };
+
+export const getIconClass = (icon) => {
+  const classes = {
+    '01d': 'fas fa-sun sun',
+    '01n': 'fas fa-moon moon',
+    '02d': 'fas fa-cloud-sun rain-sun',
+    '02n': 'fas fa-cloud-moon rain-moon',
+    '03d': 'fas fa-cloud day',
+    '03n': 'fas fa-cloud night',
+    '04d': 'fas fa-cloud-meatball day',
+    '04n': 'fas fa-cloud-meatball night',
+    '09d': 'fas fa-cloud-showers-heavy day',
+    '09n': 'fas fa-cloud-showers-heavy night',
+    '10d': 'fas fa-cloud-sun-rain rain-sun',
+    '10n': 'fas fa-cloud-moon-rain rain-moon',
+    '11d': 'fas fa-bolt bolt-day',
+    '11n': 'fas fa-bolt bolt-night',
+    '13d': 'far fa-snowflake day',
+    '13n': 'far fa-snowflake night',
+    '50d': 'fas fa-smog day',
+    '50n': 'fas fa-smog night',
+  };
+  return classes[icon];
+};
