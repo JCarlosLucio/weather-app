0d08cd90c8c5f7d54b9fc5b32f95bb1f02a6a3fa
jcLucio <c.lucio3@gmail.com>
2020-08-10 13:14:06 -0500

    Import ErrorMessage.scss

--- src/ErrorMessage.js
+++ src/ErrorMessage.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './ErrorMessage.scss';
 
 function ErrorMessage({ error }) {
   return (
-    <div>
+    <div className="ErrorMessage">
       <i className="fas fa-grin-beam-sweat" alt-label="error icon" />
       <h3>{error}</h3>
     </div>
