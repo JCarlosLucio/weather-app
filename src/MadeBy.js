3bd5fdb306b552f55cb05e75bf37e53c9479979c
jcLucio <c.lucio3@gmail.com>
2020-08-11 17:56:09 -0500

    Add MadeBy Component

---
+++ src/MadeBy.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MadeBy.scss';
+
+function MadeBy({ link }) {
+  return (
+    <div className="MadeBy">
+      <a href={link}>
+        <i className="fab fa-github-alt" aria-hidden="true" />
+      </a>
+    </div>
+  );
+}
+
+export default MadeBy;
