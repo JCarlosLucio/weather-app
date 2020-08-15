73e976e7ed9d7759a9861d3c6760d99e1d44958e
jcLucio <c.lucio3@gmail.com>
2020-08-14 19:26:35 -0500

    Add isLoading state in useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,21 +2,24 @@ import { useState, useEffect } from 'react';
 
 function useFetch(url) {
   const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(
     () => {
       if (!url) return;
       const fetchData = async () => {
+        setIsLoading(true);
         const response = await fetch(url);
         const data = await response.json();
         setData(data);
+        setIsLoading(false);
       };
       fetchData();
     },
     [url]
   );
 
-  return { data };
+  return { data, isLoading };
 }
 
 export default useFetch;
