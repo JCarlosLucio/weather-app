195aed20092a38726af04da4fead7049d31ab7e2
jcLucio <c.lucio3@gmail.com>
2020-08-14 19:29:39 -0500

    Add errorMessage state for error handling

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,6 +4,7 @@ function useFetch(url) {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [hasError, setHasError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('Something went wrong...');
 
   useEffect(
     () => {
@@ -14,10 +15,12 @@ function useFetch(url) {
         try {
           const response = await fetch(url);
           const data = await response.json();
+          if (data.cod !== 200) throw new Error(`${data.cod}: ${data.message}`);
           setData(data);
           setIsLoading(false);
         } catch (error) {
           setHasError(true);
+          setErrorMessage(error.message);
           console.log(error);
         }
       };
@@ -26,7 +29,7 @@ function useFetch(url) {
     [url]
   );
 
-  return { data, isLoading, hasError };
+  return { data, isLoading, hasError, errorMessage };
 }
 
 export default useFetch;
